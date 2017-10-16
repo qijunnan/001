a04203892ea2b270a49a64d7814b50b4a008176d
DESKTOP-OBJ7VVE\11959 <1195948692@qq.com>
2017-10-16 12:55:22 +0800

    10.16

--- 10/data.js
+++ 10/data.js
@@ -1,13 +1,69 @@
-;(function () {
+;(function (){
     'use strict';
-    window.s={
-        get:function (key) {
-            var json=localStorage.getItem(key);
-            return JSON.parse(json);
-        },
-        set:function (key,val) {
-            val=JSON.stringify(val);
-            return localStorage.setItem(key,val);
+    var task_list,last_id;
+    init_data();
+    // add(1);
+    function init_data(){
+        task_list=s.get('task_list');
+        last_id=s.get('last_id');
+        if(!task_list){
+            task_list=[];
+            s.set('task_list',task_list);
         }
-    };
+        if(!last_id){
+            last_id = 0;
+            s.set('last_id',last_id);
+        }
+    }
+    function add(title,completed){
+        completed = completed || false;
+        var new_task ={
+            id:s.get('last_id') + 1,
+            title:title,
+            completed:completed
+        }
+        task_list.push(new_task);
+        inc();
+        sync();
+    }
+    function inc(){
+        last_id = s.get('last_id');
+        s.set('last_id',last_id + 1);
+    }
+    function sync(){
+        s.set('task_list',task_list);
+    }
+    // function del(id){
+    //     for(var i =0;i<task_list.length;i++){
+    //         var item =task_list[i];
+    //         if(item.id == id){
+    //             task_list.splice(i,1);
+    //             sync();
+    //         }
+    //     }
+    // }
+    function find_index(id){
+        return task_list.findIndex(function (item){
+            if(item.id == id)
+                return true;
+        });
+    }
+    function update(id,title){
+        var task_index = find_index(id);
+        if(task_index === -1)return;
+        var task = task_list[task_index];
+        task.title = title;
+        sync();
+    }
+    function del(id){
+        var del_index = find_index(id);
+        if(del_index === -1)
+            return;
+        task_list.splice(del_index,4);
+        sync();
+    }
+    add('吃饭','睡觉');
+    update(2,'喝牛奶');
+    del(7,8,9,10);
+    console.log(task_list);
 })();
--- 10/main.js
+++ 10/main.js
@@ -1,36 +1,13 @@
-;(function () {
+;(function (){
     'use strict';
-    var task_list,last_id;
-    init_data();
-    function init_data() {
-        task_list = s.get('task_list');
-        last_id = s.get('last_id');
-        if(!task_list){
-            task_list = [];
-            s.set('task_list',task_list);
-        }
-        if (last_id) {
-        } else {
-            last_id = 0;
-            s.set('last_id', last_id);
-        }
-    }
-    function add(title,completed) {
-        completed = completed || false;
-        var new_task = {
-            id: s.get('last_id') + 1,
-            title:title,
-            completed: comleted
-        };
-        task_list.push(new_task);
-        inc();
-        sync();
-    }
-    function inc() {
-        var last_id = s.get('last_id');
-        s.set('last_id',last_id + 1);
-    }
-    function sync() {
-        s.set('task_list',task_list);
+    window.s={
+        get:function(key){
+            var json = localStorage.getItem(key);
+            return JSON.parse(json);
+        },
+        set:function(key,val){
+           val = JSON.stringify(val);
+           return localStorage.setItem(key,val)
+        },
     }
 })();
