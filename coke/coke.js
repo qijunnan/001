e7b4a134f347de6810a29b28049fabfd1bd94cba
qijunnan <1195948692@qq.com>
2017-11-16 17:21:45 +0800

    no message

---
+++ coke/coke.js
@@ -0,0 +1,21 @@
+// var box_list = document.getElementsByClassName("box");
+// console.log(box_list);
+// function box_stycle(box){
+//     for(var i = 0;i<box_list.length;i++){
+//         box = box_list[i];
+//         box.style.background = "#d27";
+//         box.style.height ="100px";
+//         box.style.width ="100px";
+//         box.style.display = "inline-block";
+//     }
+// }
+// box_stycle();
+// function a() {
+//     var box = document.querySelector("#box");
+//     var toggle_box = document.querySelector("#toggle-box");
+//     toggle_box.addEventListener("click",function () {
+//         box.hidden = !box.hidden;
+//     });
+// }
+// a();
+
